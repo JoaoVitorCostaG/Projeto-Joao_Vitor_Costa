8bdffbddfe895ca1e8f0332914c6f3242e43a1c3
Joao Vitor Costa <joao_vitor00@hotmail.com>
2019-09-08 16:39:16 -0300

    Criação da Classe C

---
+++ ClasseC.py
@@ -0,0 +1,15 @@
+﻿class ClasseC:
+  c1 = ''
+  c2 = ''
+  
+  def __init__ (self, c1p, c2p):
+    self.c1 = string (c1p)
+    self.c2 = int (c2p)
+  
+  def MC1(self):
+    print("Você está no método MC1")
+    
+  def MC2(self):
+    print("Você está no método MC2")
+
+  

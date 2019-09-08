cad03e842c79b03ac968a2e8a33a13ac6ac5a009
JoaoVitorCostaG <55035499+JoaoVitorCostaG@users.noreply.github.com>
2019-09-08 15:47:41 -0300

    Create ClasseA.py

---
+++ ClasseA.py
@@ -0,0 +1,25 @@
+class ClasseA:
+  a1 = ''
+  a2 = ''
+  
+  def __init__ (self, a1p, a2p):
+    self.a1 = int (a1p)
+    self.a2 = float (a2p)
+   
+  def setA1(self, valor):
+    self.a1 = int(valor)
+  
+  def getA1(self):
+    return self.a1
+    
+  def setA2(self, valor):
+    self.a2 = int(valor)
+  
+  def getA2(self):
+    return self.a2
+  
+  def MA1(self):
+    print("Você está no método MA1")
+    
+  def MA2(self):
+    print("Você está no método MA2")
